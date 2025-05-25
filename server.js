c00d781489b921269f621c93f4aaa26011f70170
siaheguelable <siaheguelable@gmail.com>
2025-05-25 11:26:17 +0200

    all where made

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 /* ******************************************
+
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
+const utilities = require('./utilities')
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,12 +27,34 @@ app.set('layout', './layouts/layout') // not at views root
 const static = require("./routes/static")
 app.use(static)
 
+// File Not Found Route - must be last route in list
+//app.use(async (req, res, next) => {
+  //next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+//});
+ 
+
+
+/* ***********************
+ * Middleware
+ * Middleware is code that runs before the route handler
+ *************************/
+
 
 // index route
-app.get('/', function(req, res)  {
-  res.render('index', { title: 'Home' });
-});
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
+
+/*app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})*/
 
 /* ***********************
  * Local Server Information
@@ -43,3 +69,6 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
+
